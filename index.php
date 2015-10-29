33321dac5bbd142d565a68d894a50de8b9c96f07
Chayne Thrash <chaynethrash@gmail.com>
2015-10-29 16:48:36 -0500

    Added main page with css file
    Added links to bootstrap and jquery

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="stylesheets/index.css">
+<title>Code Cleanup main page</title>
+</head>
+
+<body>
+
+<h1>Code Cleanup!</h1>
+
+<a href="login.php" id="login">login</a>
+
+
+</form>
+
+</body>
+
+</html>
