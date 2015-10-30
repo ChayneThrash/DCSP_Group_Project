5ca974cce9b0fd202b6c5d6136d300a25dd506d8
ChayneThrash <chaynethrash@gmail.com>
2015-10-30 10:01:46 -0500

    Added ajax request to validate user and pass. Dialog opens on failure

--- login.php
+++ login.php
@@ -5,6 +5,7 @@
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="stylesheets/login.css">
     <script src="scripts/login.js"></script>
@@ -17,7 +18,7 @@
 
 <div class="container">
   <h2>Login</h2>
-  <form role="form">
+  <form role="form" id="login">
     <div class="form-group">
       <label for="Username">username</label>
       <input type="text" class="form-control" id="user" placeholder="username">
@@ -32,6 +33,27 @@
     </div>
     <button type="submit" class="btn btn-default">Submit</button>
   </form>
+</div>
+
+<!-- Error message pop up for logging in -->
+<div id="loginErrorModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Error occurred during login</h4>
+      </div>
+        <p id="loginErrorMsg"></p>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
 </div>
 
 </body>
--- scripts/login.js
+++ scripts/login.js
@@ -1,16 +1,43 @@
 $(document).ready(function() {
 	$("#user").on("input propertychange paste", validateUser);
+	$(":submit").prop("disabled", true);
+	$("#login").submit(checkLogin);
 });
 
 function validateUser() {
 	var username = $("#user").val();
 	if (!isUserValid(username)) {
 		$("#usernameError").text("Make sure username contains no spaces and is less than 50 characters long.")
+		$(":submit").prop("disabled", true);
+		$("#loginErrorMsg").text("test");
+		$("#loginErrorModal").modal("show");
 	} else {
 		$("#usernameError").text("");
+		if(username.length == 0){ // Can't submit if field is empty.
+			$(":submit").prop("disabled", true);
+		} else {
+			$(":submit").prop("disabled", false);
+		}
 	}
 }
 
 function isUserValid(username) {
 	return (username.length <= 50) && (username.split(' ').length === 1);
+}
+
+function checkLogin() {
+	$.ajax({
+		method: "POST",
+		url: "ajax/checkLogin.php",
+		data: { username: $("#user").val(), password: $("#pwd").val() }
+	}).done(processLoginResponse);
+}
+
+function processLoginResponse(response) {
+	if (response === "success") {
+		window.location.href = "index.php";
+	} else {
+		$("#loginErrorMsg").text(response);
+		$("loginErrorDialog").dialog("open");
+	}
 }
