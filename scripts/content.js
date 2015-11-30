df0ba7b1ab8ad692640cdf3d82caca9cd82d69d6
bh998 <bh998@msstate.edu>
2015-11-29 19:33:59 -0600

    Upvote Functionality

--- scripts/content.js
+++ scripts/content.js
@@ -1,14 +1,13 @@
-function upvote(id) {
-
+function upvote() {
 	$.ajax({
 		type: "POST",
 		url: "ajax/upvote.php",
-		data: {contentid: $("#contentid").text()},
+		data: {contentid: content_id},
 		success: function(output){
 			$("#score").html(output);
 		}});
 }
 
-function downvote(id){
+function downvote(){
 	$("#score").html("2");
 }
